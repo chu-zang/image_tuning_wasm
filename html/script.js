b547d1b7199f3802351e92f81e64f7515d9298bd
nakayama <nakayama.masanori"cover-corp.com>
2023-04-10 22:00:18 +0900

    web側修正

--- html/script.js
+++ html/script.js
@@ -1,27 +1,3 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,11 +7,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const wasm_image_tuning_js_1 = __importStar(require("./pkg/wasm_image_tuning.js"));
+import init, { adjust_brightness } from './pkg/wasm_image_tuning.js';
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield (0, wasm_image_tuning_js_1.default)();
+        yield init();
         const inputImage = document.getElementById("input-image");
         const brightness = document.getElementById("brightness");
         const outputCanvas = document.getElementById("output-canvas");
@@ -44,7 +19,7 @@ function main() {
             const target = event.target;
             const file = target.files[0];
             imageBytes = yield loadImageBytes(file);
-            const adjustedBytes = (0, wasm_image_tuning_js_1.adjust_brightness)(imageBytes, parseFloat(brightness.value));
+            const adjustedBytes = adjust_brightness(imageBytes, parseFloat(brightness.value));
             const blob = new Blob([adjustedBytes], { type: "image/png" });
             const imageBitmap = yield createImageBitmap(blob);
             outputCanvas.width = imageBitmap.width;
@@ -55,7 +30,7 @@ function main() {
         brightness.addEventListener("input", () => __awaiter(this, void 0, void 0, function* () {
             if (imageBytes === null)
                 return;
-            const adjustedBytes = (0, wasm_image_tuning_js_1.adjust_brightness)(imageBytes, parseFloat(brightness.value));
+            const adjustedBytes = adjust_brightness(imageBytes, parseFloat(brightness.value));
             const blob = new Blob([adjustedBytes], { type: "image/png" });
             const imageBitmap = yield createImageBitmap(blob);
             outputCanvas.width = imageBitmap.width;
